f02141274de4e91c18b45769593cda0f30e3c4df
shibi-raj <shibi.1729@gmail.com>
2016-09-12 13:47:10 +0300

    box_lookup(): now implemented as two quereies, rather than one query and a loop.  The loop made finding lon_index corresponding to an observational datum a little slower.

--- merge_box_obs.py
+++ merge_box_obs.py
@@ -1,67 +1,109 @@
 from peewee import *
-from orm.merge_box_icoads_tables import *
 from orm.ocean_box_tables import *
 from orm.icoads_data_tables import *
+from observations_on_map import map_lons
 import sys
 import os
 
+
 def box_lookup(obs_lat,obs_lon):
-    """Look up location on grid of experimental data taken at longitude/latitude
-    values, obs_lon/obs_lat.  
-    
-    Look up starts with latitude, then longitude at the fixed latitude.
+    """Look up the box indices for a measurement taken at coordinates obs_lat 
+    and obs_lon.
     """
-    # print("     Observations",obs_lat,"(lat)",obs_lon,"(lon)","\n")
+    # retrieve latitude index
+    # t0 = time.time()
+    i_lat = Latitude.select(Latitude.lat_index).where(
+        Latitude.lat_box > obs_lat).limit(1).scalar() - 1
+    # t1 = time.time()
+    # print("total time1: ", t1-t0)
 
-    # latitude min and max 
-    lat_extrm = GridPoint\
-        .select(fn.Min(GridPoint.latitude),fn.Max(GridPoint.latitude))\
-        .scalar(as_tuple=True)
-    if not (lat_extrm[0] < obs_lat < lat_extrm[1]):
-        raise ValueError(
-            'Latitude not found on grid. Min/max interval {}, value given {}.'\
-            .format(lat_extrm,obs_lat))
+    # get latitude object corresponding to that index
+    # latitude = Latitude.get(Latitude.lat_index == i_lat)
 
-    # obs_lat latitude box index 
-    i_lat,lat = GridPoint.select(GridPoint.lat_index,GridPoint.latitude)\
-        .where(GridPoint.latitude > obs_lat)\
-        .limit(1)\
-        .scalar(as_tuple=True)
-    i_lat -= 1
+    i_lon = Longitude.select(Longitude.lon_index).join(Latitude).where(
+        Latitude.lat_index==22,
+        Longitude.lon_box>-65.0).limit(1).scalar() - 1 
 
-    # min and max longitudes at obs_lat latitude level 
-    lon_extrm_at_lat = Longitude\
-        .select(fn.Min(Longitude.longitude),fn.Max(Longitude.longitude))\
-        .join(GridPoint).where(GridPoint.lat_index == i_lat)\
-        .scalar(as_tuple=True)
-    if not (lon_extrm_at_lat[0]<obs_lon<lon_extrm_at_lat[1]):
-        raise ValueError(
-            'Longitude not found on grid. Min/max interval {}, value given {}.'\
-            .format(lon_extrm_at_lat,obs_lon))
+    # now, retrieve longitude index
+    # t2 = time.time()
+    # for lon in latitude.longitudes:
+    #     if lon.lon_box > obs_lon:
+    #         i_lon = lon.lon_index - 1
+    #         break
+    # t3 = time.time()
+    # print("total time2: ", t3-t2)
 
-    # obs_lon longitude box index
-    i_lon,lon = Longitude.select(Longitude.lon_index,Longitude.longitude)\
-        .join(GridPoint)\
-        .where(GridPoint.lat_index == i_lat and Longitude.longitude > obs_lon)\
-        .limit(1)\
-        .scalar(as_tuple=True)
-    i_lon -= 1
+    return i_lat,i_lon
 
-    return i_lon,i_lat
+import time
 
 def icoads_to_boxes():
-    db_merge.create_table(BoxObs)
+    # Created (recreate ) merged box and observations database
+    db_box.drop_table(ObsData)
+    db_box.create_table(ObsData)
 
-    i = 0
-    for ic in IcoadsData.select():
-        try:
-            print(box_lookup(ic.latitude,ic.longitude))
-            i+=1
-            print(ic.longitude,ic.latitude)
-        except:
-            pass
-        if i > 10:
-            break
+    # set up data structures for database
+    dict_obs = dict()
+    data_obs = list()
+
+    # counting indices
+    total_icoads_inserts = 0
+    icoads_errors = 0
+
+    with db_box.atomic():
+        for i, ic in enumerate(IcoadsData.select().limit(1000)):
+            try:
+                # raises error if box_lookup does not work, i.e., can't find box
+                modified_lon = map_lons(ic.lon_obs)[0]
+                name = '_'.join(map(str,box_lookup(ic.lat_obs,modified_lon)))
+                
+                # Creating list of obs data for insert
+                dict_obs.update({
+                    'name':name,
+                    'lat_obs':ic.lat_obs,
+                    'lon_obs':modified_lon,
+                    'sst':ic.sst,
+                    'date':ic.date,
+                    'pentad':ic.pentad,
+                    'half_mth':ic.half_mth}
+                )
+                data_obs.append(dict_obs.copy())
+            except:
+                icoads_errors+=1
+
+            # populating database tables
+            if i % 200 == 0 and data_obs:    
+                with db_box.atomic():
+                    ObsData.insert_many(data_obs).execute()
+                    total_icoads_inserts += len(data_obs)
+                    # print("... # inserts: ", total_icoads_inserts)
+                    # print("... # errors:  ", icoads_errors)
+                    sys.stdout.flush()
+                    data_obs[:] = []
+            # if i % 1000 == 0 and data_obs:    
+            #     ObsData._meta.auto_increment = False
+            #     with db_box.transaction():
+            #         for obsdata in data_obs:
+            #             ObsData.insert(**obsdata).execute()
+            #         total_icoads_inserts += len(data_obs)
+            #         # print("... # inserts: ", total_icoads_inserts)
+            #         # print("... # errors:  ", icoads_errors)
+            #         sys.stdout.flush()
+            #         data_obs[:] = []
+            #     ObsData._meta.auto_increment = True
+
+
+
+        # populating database tables, catch leftover
+        with db_box.atomic():
+            if data_obs:
+                ObsData.insert_many(data_obs).execute()
+                total_icoads_inserts += len(data_obs)
+
+    print("total inserts of ICOADS data: ", total_icoads_inserts)
+    print("total errors inserting data:  ", icoads_errors)
 
 if __name__ == '__main__':
     icoads_to_boxes()
+    # lat_obs = 5.0
+    # lon_obs = -150.0
